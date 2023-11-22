1ec645053950f0ddc18e73a6fb722fb63b1ad35b
nitinvarda <nitinvardha09@gmail.com>
2023-11-22 01:39:18 -0600

    update with functionality

--- controller/orderController.js
+++ controller/orderController.js
@@ -84,20 +84,49 @@ const getOrderById = asyncHandler( async function(req, res, next) {
 
 
 //@desc Update order to paid
-//@route GET /api/orders/:id/pay
+//@route PUT /api/orders/:id/pay
 //@access Private
 
 const updateOrderToPaid = asyncHandler( async function(req, res, next) {
-    res.send('update order to paid')
+    const order = await Order.findById(req.params.id);
+    if(order){
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult ={
+            id:req.body.id,
+            status:req.body.status,
+            update_time:req.body.update_time,
+            email_address:req.body.payer.email_address
+        }
+
+        const updateOrder = await order.save();
+        res.status(200).json(updateOrder);
+    }
+    else{
+        res.satus(404);
+        throw new Error("Order not found");
+    }
     
  })
 
  //@desc Update to delivered
-//@route GET /api/orders/:id/deliver
+//@route PUT /api/orders/:id/deliver
 //@access Private
 
 const updateOrderToDelivered = asyncHandler( async function(req, res, next) {
-    res.send('update order to delivered')
+    const order = await Order.findById(req.params.id);
+    if(order){
+        order.isDelivered = true;
+        order.deliveredAt = Date.now();
+
+        const updateOrder = await order.save();
+        res.status(200).json(updateOrder);
+    }
+    else{
+        res.status(404);
+        throw new Error("Order not found");
+    }
+
     
  })
 
@@ -106,7 +135,8 @@ const updateOrderToDelivered = asyncHandler( async function(req, res, next) {
 //@access Private/Admin
 
 const getOrders = asyncHandler( async function(req, res, next) {
-    res.send('get all orders')
+  const orders = await Order.find({}).populate('user','id name');
+  res.status(200).json(orders);
     
  })
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,9 +3,15 @@ import Product from '../models/productModel.js';
 var router = Router();
 import asyncHandler from '../middleware/asyncMiddleware.js';
 import { getAllProducts, getProductById } from '../controller/productController.js';
+import MulterGridfsStorage from 'multer-gridfs-storage';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
-/* GET home page. */
-router.get('/',getAllProducts);
+
+
+router.get("/",getAllProducts)
+  
 router.get("/:id",getProductById)
  
 export default router;
